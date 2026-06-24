35f3699f54f1fc1b375265a13f55c4439faf52e5
macbua <unreal_sound@mail.ru>
2026-06-25 03:33:18 +0700

    background phonebook without frontend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,70 +1,63 @@
 const express = require('express')
-const path = require('path');
-const fs = require('fs')
+require('dotenv').config()
 const morgan = require('morgan')
 const cors = require('cors')
-const config = require('./config.json');
+const Person = require('./models/person')
+const config = require('./config.json')
+
 const app = express()
 
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(config.loggingFormat))
-let persons = require('./db.json');
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(people => {res.json(people)}).catch(err => next(err))})
+
+app.get('/info', (req, res, next) => {
+  Person.find({}).then(people => {res.send(`<p>Phonebook has info for ${people.length} people</p>
+    <p>${new Date().toString()}</p>`)}).catch(err => next(err))})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then(person => {
+    if(!person) return res.status(404).end()
+    res.json(person)}).catch(err => next(err))
 })
 
-app.get('/info', (req, res) => {
-        res.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date().toString()}</p>`)
-})
+app.post('/api/persons', (req, res, next) => {
+  const newPerson = new Person({ name: req.body.name?.trim(), number: req.body.number?.trim() })
+  newPerson.save().then(saved => res.json(saved)).catch(err => next(err))})
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const pers = persons.find(item => item.id === id);
-    if (pers) res.send(pers)
-        else res.status(404).end()
-})
+app.put('/api/persons/:id', async (req, res, next) => {
+  try{
+    const { number } = req.body
+    const findPerson = await Person.findById(req.params.id)
+    if(!findPerson) return res.status(404).json({ err: 'User not found' })
+    findPerson.number = number?.trim()
+    const updatedPerson = await findPerson.save()
+    res.json(updatedPerson)}
+  catch(err){next(err)}})
 
-app.put('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)    
-    const { name, number } = req.body
-    if (!name || !number) {
-        return res.status(400).json({ error: 'Name and number are required' })
-    }
-    const updatedPerson = {
-        id,
-        name: name.trim(),
-        number: number.trim()
-    }
-    persons = persons.map(item => item.id === id ? updatedPerson : item)
-    res.json(updatedPerson)
-})
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(_ => {res.status(204).end()}).catch(err => next(err))})
 
-app.delete('/api/persons/:id', (req, res) => { 
-    const id = req.params.id
-    persons = persons.filter(item => item.id !== id)
-    res.status(204).end()
-})
-
-app.post('/api/persons', (req, res) => {
-    const { name, number } = req.body
-    if (!name || !number) {
-        return res.status(400).json({ error: 'Name and number are required' })
-    }
-    if(persons.some(item => item.name === name.trim())) {
-        return res.status(400).json({ error: 'Name must be unique' })
-    }
-    const newPerson = {
-        id: Math.floor(Math.random() * 1000000).toString(),
-        name: name.trim(),
-        number: number.trim()
-    }
-    persons = [...persons, newPerson]
-    res.status(201).json(newPerson)
+app.use((err, req, res, _next) => {
+  console.error(err.message)
+  console.error(err.stack)
+  if(err.name === 'CastError') {
+    return res.status(400).json({ err: 'malformatted id' })
+  }else if(err.name === 'ValidationError'){
+    return res.status(400).json({ err: err.message })
+  }
+  else if(err.code === 11000){
+    return res.status(409).json({ err: 'duplicate key' })
+  }else if(err.name === 'SyntaxError'){
+    return res.status(400).json({ err: 'incorrect syntax' })
+  }
+  return res.status(500).json({ err: err.message })
 })
 
 const PORT = process.env.PORT || 3001
